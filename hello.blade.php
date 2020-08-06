63976d2b120ab5df15a9ca4956af97d5c5da907d
Ayush Kumar <68781382+Ares225@users.noreply.github.com>
2020-08-06 21:46:59 +0530

    Add files via upload

---
+++ hello.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <title>Hello View</title>
+</head>
+<body>
+
+      <h1>Hello All.whats up?</h1>
+      <?php print_r($mysub); ?>  <br>
+      <?php print_r($marks); ?>
+</body>
+</html>
